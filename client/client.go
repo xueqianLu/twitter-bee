eadf2358622295f7e9377336825f21e2cded459c
luxq <xueqian1991@gmail.com>
2025-01-12 00:20:52 +0800

    add log

--- client/client.go
+++ client/client.go
@@ -31,26 +31,31 @@ func (c *BeeClient) GetFollowerCount(userID string) (*apimodels.FollowerCountRes
 	url := fmt.Sprintf("%s/tbapi/v1/follower/count", c.BaseURL)
 	reqBody, err := json.Marshal(apimodels.FollowerCountRequest{UserId: userID})
 	if err != nil {
+		fmt.Println("GetFollowerCount json.Marshal error:", err)
 		return nil, err
 	}
 
 	resp, err := c.HTTPClient.Post(url, "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
+		fmt.Println("GetFollowerCount HTTPClient.Post error:", err)
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
+		fmt.Println("GetFollowerCount unexpected status code:", resp.StatusCode)
 		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
 	var result Response
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		fmt.Println("GetFollowerCount decode http result error:", err)
 		return nil, err
 	}
 
 	var data apimodels.FollowerCountResponse
 	if err := json.Unmarshal(result.Data, &data); err != nil {
+		fmt.Println("GetFollowerCount json.Unmarshal result error:", err)
 		return nil, err
 	}
 	return &data, nil
@@ -60,25 +65,30 @@ func (c *BeeClient) GetFollowerList(user string, cursor string) (*apimodels.Foll
 	url := fmt.Sprintf("%s/tbapi/v1/follower/list", c.BaseURL)
 	reqBody, err := json.Marshal(apimodels.FollowerListRequest{User: user, Cursor: cursor})
 	if err != nil {
+		fmt.Println("GetFollowerList json.Marshal error:", err)
 		return nil, err
 	}
 
 	resp, err := c.HTTPClient.Post(url, "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
+		fmt.Println("GetFollowerList HTTPClient.Post error:", err)
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
+		fmt.Println("GetFollowerList unexpected status code:", resp.StatusCode)
 		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
 	var result Response
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		fmt.Println("GetFollowerList decode http result error:", err)
 		return nil, err
 	}
 	var data apimodels.FollowerListResponse
 	if err := json.Unmarshal(result.Data, &data); err != nil {
+		fmt.Println("GetFollowerList json.Unmarshal result error:", err)
 		return nil, err
 	}
 	return &data, nil
